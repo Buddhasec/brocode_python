76bc074052c9fee35047651d70e53b3e68ebebeb
Buddhasec <Buddhasec@github.com>
2025-07-07 17:52:38 +0200

    Arithmetik operations and augmented assigments (shorter code)

---
+++ arithmetik_overview_and_augmented_assignment.py
@@ -0,0 +1,31 @@
+# Here we set a variable of friends to 0
+friends = 0
+
+# Increas the value of 'friends' by 1 (long version).
+# Also known as normal assignment with addition.
+friends = friends + 1
+
+# Same operation using a shorthand operator.
+# Also known as compound assignment or augmented assignment.
+friends += 1
+
+# This works for other operations as well:
+
+# Subtraction (minus)
+friends -= 1
+
+# Multiplication (times)
+friends *= 1
+
+# Divison (splitting into equal parts)
+friends /= 1
+
+# Exponentiation (raising to a power)
+
+friends **= 1
+
+# since the code runs top to bottom,
+# each operation is applied one after another,
+# and the final result is stored in the variable.
+
+print(friends)
