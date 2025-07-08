8bb56a6d76a3d96050d4b6a43c454b95ad81a6b7
Buddhasec <Buddhasec@github.com>
2025-07-08 10:04:48 +0200

    Program that calculates the circumference of a circle with user input, using the rounding method of :.2f for exact calculation

---
+++ circumference_of_a_circle.py
@@ -0,0 +1,14 @@
+# First, import the math module to access advanced math functions.
+import math
+
+# Get the radius from the user, typecast it to a float.
+radius = float(input("Enter the radius (can be a whole number or decimal): "))
+
+# Calculate the circumference using the formula: 2 * pi * radius.
+# math.pi provides the value of pi.
+# The typecast of round(, 2) rounds to 2 decimal places in the variable to
+# save it for further calculations.
+circumference = round(2 * math.pi * radius, 2)
+
+# Print the result using an f-string, rounded to 2 decimal places.
+print(f"The circumference is: {circumference}.")
