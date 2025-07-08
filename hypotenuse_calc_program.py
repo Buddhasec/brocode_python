023bb8d4a3ee3a61dbf8a84444f571973aa31391
Buddhasec <Buddhasec@github.com>
2025-07-08 14:21:12 +0200

    This is a calculator for the hypotenuse rounded to 2 decimal places

---
+++ hypotenuse_calc_program.py
@@ -0,0 +1,14 @@
+import math
+
+# The formula to calculate the hypotenuse is: c = sqrt(a² + b²).
+# We need two input values, side A and side B, to calculate side C.
+
+length1 = float(input("Please enter the length of side A: "))
+length2 = float(input("Please enter the length of side B: "))
+
+# It is important to use parantheses around the full expression,
+# to ensure that the square root applies to the entire sum.
+length3 = math.sqrt(pow(length1, 2) + pow(length2, 2))
+
+# Print the result, optionally rounded to 2 decimal places.
+print(f"The length of side c (the hypotenuse) is: {length3:.2f}")
