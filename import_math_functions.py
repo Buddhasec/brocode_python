f453f31f47ba66730b6ff67666d9ad6d4181cf4d
Buddhasec <Buddhasec@github.com>
2025-07-08 08:48:27 +0200

    Imported the math module and called math.function_name()

---
+++ import_math_functions.py
@@ -0,0 +1,25 @@
+# To use advanced math functions, you have to import the 'math' module.
+import math
+
+# You can directly call math functions using math.function_name().
+# For example, math.pi is a constant used to calculate circles ()
+print(math.pi)
+
+# math.e is another mathematical constant (Euler's number),
+# commonly used in physics and for exponential growth calculations.
+print(math.e)
+
+# Example: Calculating the square root of an integer.
+x = 16
+result1 = math.sqrt(x)
+print(result1) # Output: 4.0
+
+# Example: Always rounding a float up using math.ceil().
+y = 9.1
+result2 = math.ceil(y)
+print(result2) # Output: 10
+
+# Example: Always rounding a float down using math.floor()-
+z = 9.9
+result3 = math.floor(z)
+print(result3) # Output: 9
