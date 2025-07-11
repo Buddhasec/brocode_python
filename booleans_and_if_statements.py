7e71c0866617baa6e47ca0a836e6255dbf3d4766
Buddhasec <Buddhasec@github.com>
2025-07-11 14:37:09 +0200

    A combination of Booleans and if statements

---
+++ booleans_and_if_statements.py
@@ -0,0 +1,20 @@
+# Booleans can only be True or False.
+# A condition is only executed if the Boolean is True.
+
+# Set the Boolean variable to False.
+for_sale = False
+
+# Check if the item is for sale.
+if for_sale:
+    print("This item IS for sale.")
+else:
+    print("This item is NOT for sale.")
+
+# Set another Boolean variabel to True.
+online = True
+
+# Check if the user is currently online.
+if online:
+    print("The user IS currently online.")
+else:
+    print("The user is currently NOT online")
